be12f36df7dd17bfa4ffb079cfea372e2d51379e
Antonio Liccardo <tuxmal@tiscali.it>
2014-05-01 21:22:23 +0200

    Creating some test.

---
+++ test/template.rb
@@ -0,0 +1,15 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+$:.unshift File.join(File.dirname(__FILE__),'..','lib')
+
+require 'test/unit'
+require 'template'
+
+class Template < Test::Unit::TestCase
+  def test_foo
+    #TODO: Write test
+    flunk "TODO: Write test"
+    # assert_equal("foo", bar)
+  end
+end
---
+++ test/ts_suite.rb
@@ -0,0 +1,11 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require 'test/unit' 
+
+# Add your testcases here
+
+require 'tc_1' 
+require 'tc_2' 
+require 'tc_3' 
+
