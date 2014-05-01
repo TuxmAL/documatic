c9153f971dc0bf98a77d4417ebb19336e582a3f6
Antonio Liccardo <tuxmal@tiscali.it>
2014-05-01 21:22:23 +0200

    Creating some test.

---
+++ test/test_odt.rb
@@ -0,0 +1,18 @@
+# DOCUMATIC TUTORIAL
+#   -- by Dave Nelson <urbanus@240gl.org>
+
+# This tutorial is a trivial demonstration of how Documatic works.  By
+# all means play with this file: fiddle with the data, see what
+# happens.
+
+# We start by requiring the 'documatic' rubygem, which also loads
+# Ruport.
+
+require '../lib/documatic'
+
+Options = Struct.new(:template_file, :output_file)
+opts = Options.new
+opts.template_file = 'template.odt'
+opts.output_file   = 'output/tested.odt'
+text = 'Text-from-Ruby'
+Documatic::OpenDocumentText::Template::process_template(:options => opts, :data => [text])
