b96944eeee7c6b82dd077c9ce9546f4ebba8e00f
Antonio Liccardo <tuxmal@tiscali.it>
2014-05-05 15:36:12 +0200

    Removed unneded output statement.

--- test/test_odt.rb
+++ test/test_odt.rb
@@ -4,10 +4,11 @@
 $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__ + '/../../lib/documatic'))
 
 require 'documatic'
-puts "$LOAD_PATH=#{$LOAD_PATH}."
+
 Options = Struct.new(:template_file, :output_file)
 opts = Options.new
 opts.template_file = 'template.odt'
 opts.output_file   = 'output/tested.odt'
 text = 'Text-from-Ruby'
+
 Documatic::OpenDocumentText::Template::process_template(:options => opts, :data => [text])
